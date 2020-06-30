ea3df59982f11945cd9b4605ffa70a1be3bd1c75
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-06-30 15:37:15 +0800

    Added more debug messages

--- extract_units.py
+++ extract_units.py
@@ -15,8 +15,14 @@ def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
 
     nan_set = set([np.nan])
     results = defaultdict(set)
+    iterator = pd.read_csv(
+        RAW_FILE,
+        iterator=True,
+        chunksize=chunksize,
+        low_memory=False
+    )
 
-    for i, chunk in enumerate(pd.read_csv(RAW_FILE, iterator=True, chunksize=chunksize)):
+    for i, chunk in enumerate(iterator):
         print(f'INFO - Processing chunk#{i}')
 
         # item ID is the feature ID
@@ -25,9 +31,11 @@ def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
 
         # group df by item ID
         # list all of the possible units of each item ID
+        print('INFO - Grouping items with same ITEMID...')
         group = chunk[columns].groupby('ITEMID')
         agg = group['VALUEUOM'].apply(list)
 
+        print('INFO - Updating results from groups...')
         for key in agg.keys():
             values = set(agg[key])
             results[key].update(values)
@@ -38,6 +46,7 @@ def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
         os.mkdir('pickled_objects')
 
     # pickle results for later use
+    print('INFO - Writing output file...')
     output_file = os.path.join('pickled_objects', output_file)
     with open(output_file, 'wb') as fd:
         pickle.dump(results, fd)
