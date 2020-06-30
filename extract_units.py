d76b6064141d8d3a39eefc7f6dbdab7b472e38b1
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-06-30 12:53:13 +0800

    Modified extract unit scripts

--- extract_units.py
+++ extract_units.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+import fire
 import os
 import pandas as pd
 import pickle
@@ -7,17 +8,36 @@ import pickle
 ROOT = 'mimic_database'
 RAW_FILE = os.path.join(ROOT, 'CHARTEVENTS.csv')
 
-results = defaultdict(set)
-for chunk in pd.read_csv(RAW_FILE, iterator=True, chunksize=10_000_000):
-    x = chunk[['ITEMID', 'VALUEUOM']].groupby(
-        'ITEMID')['VALUEUOM'].apply(list)
-    for k in x.keys():
-        values = set(x[k])
-        results[k].update(values)
 
-        if len(results[k]) != 1:
-            raise AssertionError(f'Units for ID="{k}" has many: {results[k]}')
+def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
+    results = defaultdict(set)
+    for chunk in pd.read_csv(RAW_FILE, iterator=True, chunksize=chunksize):
+        # item ID is the feature ID
+        # valueuom is the unit used by the feature
+        columns = ['ITEMID', 'VALUEUOM']
 
-output_file = os.path.join('pickled_objects', 'feature_units.p')
-with open(output_file, 'wb') as fd:
-    pickle.dump(results, fd)
+        # group df by item ID
+        # list all of the possible units of each item ID
+        group = chunk[columns].groupby('ITEMID')
+        agg = group['VALUEUOM'].apply(list)
+
+        for key in agg.keys():
+            values = set(agg[key])
+            results[key].update(values)
+
+            if len(results[key]) != 1:
+                raise AssertionError(
+                    f'Units for ID="{key}" has many: {results[key]}')
+
+    # ensure output dir exists
+    if not os.path.isdir('picked_objects'):
+        os.mkdir('pickled_objects')
+
+    # pickle results for later use
+    output_file = os.path.join('pickled_objects', output_file)
+    with open(output_file, 'wb') as fd:
+        pickle.dump(results, fd)
+
+
+if __name__ == '__main__':
+    fire.Fire(extract_units)
