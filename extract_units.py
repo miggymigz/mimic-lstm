2b5e86aa412b6e63ffd39ec2e81a3148317391df
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-07-01 15:19:21 +0800

    Optimized script

--- extract_units.py
+++ extract_units.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from functools import reduce
+from process_mimic import ParseItemID
 from tqdm import tqdm
 
 import fire
@@ -15,14 +16,18 @@ N_ROWS = 330_712_483
 
 def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
     total = math.ceil(N_ROWS / chunksize)
-    nan_set = set([np.nan])
-    results = defaultdict(set)
+    results = {}
     iterator = pd.read_csv(
         RAW_FILE,
         iterator=True,
         chunksize=chunksize,
         low_memory=False
     )
+
+    pid = ParseItemID()
+    pid.build_dictionary()
+    feature_ids = pid.dictionary.values()
+    feature_ids = reduce(lambda x, y: x.union(y), feature_ids)
 
     with tqdm(total=total, position=0) as pbar:
         for i, chunk in enumerate(iterator):
@@ -43,15 +48,20 @@ def extract_units(chunksize=10_000_000, output_file='feature_units.p'):
             for j, key in enumerate(keys):
                 pbar.set_description(
                     f'Updating results of chunk#{i}: {j+1}/{len(keys) + 1}')
-                values = set(agg[key])
-                results[key].update(values)
-                results[key] = results[key].difference(nan_set)
+
+                if key in feature_ids:
+                    values = set(agg[key])
+
+                    if key in results:
+                        results[key].update(values)
+                    else:
+                        results[key] = values
 
             # update tqdm progress
             pbar.update()
 
     # ensure output dir exists
-    if not os.path.isdir('picked_objects'):
+    if not os.path.isdir('pickled_objects'):
         os.mkdir('pickled_objects')
 
     # pickle results for later use
