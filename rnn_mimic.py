d1d69731e6ebf862e2dc088776270866a776172e
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-07-08 12:06:14 +0800

    Refined postprocessing steps

--- rnn_mimic.py
+++ rnn_mimic.py
@@ -30,8 +30,20 @@ from sklearn.model_selection import StratifiedKFold
 
 ROOT = os.path.join('mimic_database', 'mapped_elements')
 PREPROCESSED_FILE = os.path.join(ROOT, 'CHARTEVENTS_preprocessed.csv')
-PREPROCESSED_FILE_AGE = os.path.join(
-    ROOT, 'CHARTEVENTS_preprocessed_validages.csv')
+
+
+# feature IDs useful for post processing
+ID_AGE = {
+    'MI': 145,
+    'SEPSIS': 149,
+    'VANCOMYCIN': 149,
+}
+ID_MEDS_START = {
+    'MI': 147,
+    'SEPSIS': 151,
+    'VANCOMYCIN': 151,
+}
+
 
 ######################################
 ## MAIN ###
@@ -116,8 +128,7 @@ def return_data(synth_data=False, balancer=True, target='MI',
         Y_TRAIN = np.vstack(y_train)
 
     else:
-        # df = pd.read_csv(PREPROCESSED_FILE, low_memory=False)
-        df = pd.read_csv(PREPROCESSED_FILE_AGE, low_memory=False)
+        df = pd.read_csv(PREPROCESSED_FILE, low_memory=False)
 
         if target == 'MI':
             df[target] = ((df['troponin'] > 0.4) & (
@@ -201,10 +212,8 @@ def return_data(synth_data=False, balancer=True, target='MI',
         MATRIX = MATRIX.reshape(
             int(MATRIX.shape[0]/time_steps), time_steps, MATRIX.shape[1])
 
-        # zero out padding days which have zero values anywhere except "meds" group
-        _meds_start = get_meds_start(target)
-        _mask = ~MATRIX[:, :, :_meds_start].any(axis=2)
-        MATRIX[_mask] = 0
+        # add my own preprocessing steps
+        MATRIX = preprocess_matrix(MATRIX, target)
 
         # keep a copy of the original dataset
         ORIG_MATRIX = np.copy(MATRIX)
@@ -317,17 +326,18 @@ def return_data(synth_data=False, balancer=True, target='MI',
                 np.concatenate((Y_TRAIN, Y_VAL), axis=0), no_feature_cols)
 
 
-def get_meds_start(target):
-    if target == 'MI':
-        return 147
+def preprocess_matrix(matrix, target):
+    # zero out padding days which have zero values anywhere except "meds" group
+    _meds_start = ID_MEDS_START[target]
+    _mask = ~matrix[:, :, :_meds_start].any(axis=2)
+    matrix[_mask] = 0
 
-    if target == 'SEPSIS':
-        return 151
+    # remove samples with age > 90
+    _id_age = ID_AGE[target]
+    _temp = matrix.transpose(0, 2, 1)[:, _id_age, :]
+    matrix = _temp[~(_temp.max(axis=1) > 90)]
 
-    if target == 'VANCOMYCIN':
-        return 151
-
-    raise AssertionError(f'Unsupported target: {target}')
+    return matrix
 
 
 def build_model(no_feature_cols=None, time_steps=7, output_summary=False):
@@ -358,15 +368,15 @@ def build_model(no_feature_cols=None, time_steps=7, output_summary=False):
     # RMS = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
     # model.compile(optimizer=RMS, loss='binary_crossentropy', metrics=['acc'])
 
-    # optimizer = tf.keras.optimizers.RMSprop(
-    #     learning_rate=0.001,
-    #     rho=0.0,
-    #     epsilon=1e-08,
-    # )
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=0.001,
+        rho=0.0,
+        epsilon=1e-08,
+    )
 
     model = Mimic3Lstm(no_feature_cols, time_steps)
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        optimizer=optimizer,  # tf.keras.optimizers.Adam(learning_rate=0.001),
         loss='binary_crossentropy',
         metrics=['acc'],
     )
