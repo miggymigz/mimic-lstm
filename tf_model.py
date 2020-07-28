4fb01ded00e5552598bc775e5d3e1e7c1c199106
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-07-28 22:49:19 +0800

    Added optional attn masking to LSTM model

--- tf_model.py
+++ tf_model.py
@@ -1,37 +1,51 @@
 import tensorflow as tf
 
 
+def create_attention_mask(x):
+    x = tf.reduce_any(x != 0, axis=-1)
+    x = x[:, :, tf.newaxis]
+    x = tf.cast(x, tf.float32)
+    return (1 - x) * -1e9
+
+
 class Attention(tf.keras.layers.Layer):
-    def __init__(self, n_features, time_steps=14, **kwargs):
+    def __init__(self, time_steps=14, mask=False, **kwargs):
         super().__init__(**kwargs)
 
-        self.n_features = n_features
-        self.time_steps = time_steps
+        self.mask = mask
 
         self.dense = tf.keras.layers.Dense(
             time_steps,
-            activation='softmax',
-            input_shape=(n_features, time_steps),
+            activation=None,
         )
 
-    def call(self, inputs):
-        a_weights = tf.transpose(inputs, perm=(0, 2, 1))
-        a_weights = self.dense(a_weights)
-        a_weights = tf.transpose(a_weights, perm=(0, 2, 1))
-        x = tf.multiply(inputs, a_weights)
-        return x, a_weights
+    def call(self, x):
+        w = tf.transpose(x, perm=(0, 2, 1))
+        w = self.dense(w)
+        w = tf.transpose(w, perm=(0, 2, 1))
+
+        # apply mask to padding days
+        if self.mask:
+            mask = create_attention_mask(x)
+            w = w + mask
+
+        # softmax scores and apply attention weights
+        w = tf.nn.softmax(w, axis=-2)
+        x = tf.multiply(x, w)
+
+        return x, w
 
 
 class Mimic3Lstm(tf.keras.Model):
-    def __init__(self, n_features, time_steps=14, batch_size=1, **kwargs):
+    def __init__(self, n_features, time_steps=14, mask=False, batch_size=1, **kwargs):
         super().__init__(**kwargs)
 
         self.n_features = n_features
         self.time_steps = time_steps
 
         self.attention = Attention(
-            n_features,
             time_steps=time_steps,
+            mask=mask,
             batch_input_shape=(batch_size, time_steps, n_features)
         )
         self.masking = tf.keras.layers.Masking(
@@ -46,11 +60,11 @@ class Mimic3Lstm(tf.keras.Model):
         )
 
     def call(self, inputs):
-        x, a_weights = self.attention(inputs)
+        x, w = self.attention(inputs)
         x = self.masking(x)
         x = self.lstm(x)
-        preds = self.distributed(x)
-        return preds, a_weights
+        x = self.distributed(x)
+        return x, w
 
     def model(self):
         x = tf.keras.layers.Input(shape=(self.time_steps, self.n_features))
